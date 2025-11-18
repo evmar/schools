f12e429d6bcfb4af7258a381c25c6d69ee86eb82
Evan Martin <evan.martin@gmail.com>
2025-11-18 13:34:48 -0800

    english/math switcher

--- web/app.js
+++ web/app.js
@@ -1,6 +1,6 @@
 import DATA from "./data.js";
 
-var map = L.map("map").setView([44.0994213, -120.6629185], 7);
+const map = L.map("map").setView([44.0994213, -120.6629185], 7);
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   minZoom: 6,
   maxZoom: 19,
@@ -8,34 +8,67 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-const numbers = L.layerGroup();
-for (const s of DATA) {
-  if (!s.lat) continue;
-  const score = s.subjects?.english;
-  if (!score) continue;
+let curLayer;
+function render(data, subject) {
+  if (curLayer) {
+    map.removeLayer(curLayer);
+  }
 
-  const circle = L.circleMarker([s.lat, s.long], {
-    color: "none",
-    fillColor: `hsl(${score} 100% 40%)`,
-    fillOpacity: 0.7,
-    radius: 20,
-  })
-    .addTo(map)
-    .bindPopup(`${s.name}: ${score}`);
+  const layer = L.layerGroup();
 
-  const text = L.marker([s.lat, s.long], {
-    icon: L.divIcon({
-      className: "circle-text",
-      html: `<span>${score.toFixed(0)}</span>`,
-    }),
-    interactive: false,
-  }).addTo(numbers);
+  const circles = L.layerGroup().addTo(layer);
+  const numbers = L.layerGroup();
+  for (const s of data) {
+    if (!s.lat) continue;
+    const score = s.subjects[subject];
+    if (!score) continue;
+
+    const circle = L.circleMarker([s.lat, s.long], {
+      color: "none",
+      fillColor: `hsl(${score} 100% 40%)`,
+      fillOpacity: 0.7,
+      radius: 20,
+    })
+      .addTo(circles)
+      .bindPopup(`${s.name}: ${score}`);
+
+    const text = L.marker([s.lat, s.long], {
+      icon: L.divIcon({
+        className: "circle-text",
+        html: `<span>${score.toFixed(0)}</span>`,
+      }),
+      interactive: false,
+    }).addTo(numbers);
+  }
+
+  function zoomEnd() {
+    if (map.getZoom() < 12) {
+      layer.removeLayer(numbers);
+    } else {
+      layer.addLayer(numbers);
+    }
+  }
+  map.on("zoomend", zoomEnd);
+  layer.on("remove", () => map.off("zoomend", zoomEnd));
+  zoomEnd();
+  map.addLayer(layer);
+  curLayer = layer;
 }
 
-map.on("zoomend", () => {
-  if (map.getZoom() < 12) {
-    map.removeLayer(numbers);
-  } else {
-    map.addLayer(numbers);
-  }
+render(DATA, "math");
+
+const x = L.Control.extend({
+  onAdd(map) {
+    const sel = document.createElement("select");
+    for (const f of ["math", "english"]) {
+      const opt = document.createElement("option");
+      opt.innerText = f;
+      sel.appendChild(opt);
+    }
+    L.DomEvent.on(sel, "change", () => {
+      render(DATA, sel.value);
+    });
+    return sel;
+  },
 });
+new x({ position: "topright" }).addTo(map);
